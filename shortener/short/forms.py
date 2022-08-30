fb61281f824887ed8f4d8bacb8826c2b3f5ce01d
Daniil <dan.dad@bk.ru>
2022-08-30 21:03:37 +0300

    created form and html page

--- shortener/short/forms.py
+++ shortener/short/forms.py
@@ -1,1 +1,9 @@
 from django.forms import ModelForm
+from .models import Link
+
+
+class ShortForm(ModelForm):
+    
+    class Meta:
+        model = Link
+        fields = ('url',)
--- shortener/short/views.py
+++ shortener/short/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from .forms import ShortForm
 
 class ShortenerView(View):
     
     def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello World!")
+        form = ShortForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'shortener.html', context)
     
     def post(self, request, *args, **kwargs):
+        return HttpResponse("Hello World")
+
+
+class RedirectUrl(View):
+    
+    def get(self, request, *args, **kwargs):
         pass
